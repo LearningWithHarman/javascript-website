ede82859db9b74d14d324d96504d796ffcc7f437
LearningWithHarman <159704121+LearningWithHarman@users.noreply.github.com>
2024-04-08 18:11:38 +0530

    Add files via upload

---
+++ 30_script.js
@@ -0,0 +1,45 @@
+const videoElement = document.querySelector('.container section video');
+
+videoElement.addEventListener('animationend', () => {
+  videoElement.classList.add('ended');
+});
+
+
+const section1Element = document.getElementById('ok');
+
+section1Element.addEventListener('animationend', () => {
+  section1Element.style.opacity = '1'; // Set opacity back to 1
+});
+
+
+const boxes = document.querySelectorAll('.box');
+window.addEventListener('scroll', checkboxes);
+checkboxes();
+function checkboxes() {
+  const tbottom = window.innerHeight / 5 * 4;
+
+  boxes.forEach(box => {
+    const boxtop = box.getBoundingClientRect().top;
+
+    if (boxtop < tbottom) {
+      box.classList.add('show');
+    }
+    else {
+      box.classList.remove('show');
+    }
+
+  })
+}
+
+function navbar() {
+  document.getElementById('navjar').style.left = '0';
+}
+function backfire() {
+  document.getElementById('navjar').style.left = '-600%';
+}
+function navbar2() {
+  document.getElementById('navter').style.left = '0';
+}
+function backfire2() {
+  document.getElementById('navter').style.left = '-600%';
+}
